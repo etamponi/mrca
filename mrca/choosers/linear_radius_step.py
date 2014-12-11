98a69827793fba71058136b548e8a9b48ad3e84c
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-12-11 18:26:09 +0100

    LinearRadiusStep

---
+++ mrca/choosers/linear_radius_step.py
@@ -0,0 +1,21 @@
+import numpy
+from mrca import choosers
+
+__author__ = 'Emanuele Tamponi'
+
+
+class LinearRadiusStep(object):
+
+    def __init__(self, finder, smallest_size, largest_size, profile_dim):
+        self.finder = finder
+        self.smallest_size = smallest_size
+        self.largest_size = largest_size
+        self.profile_dim = profile_dim
+
+    def choose(self, inputs):
+        smallest_size = choosers.absolute_size(self.smallest_size, inputs)
+        largest_size = choosers.absolute_size(self.largest_size, inputs)
+        smallest_radius = self.finder(smallest_size, inputs)
+        largest_radius = self.finder(largest_size, inputs)
+        radii = numpy.linspace(smallest_radius, largest_radius, self.profile_dim).tolist()
+        return radii
---
+++ mrca/choosers/test_linear_radius_step.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy
+from mrca.choosers.linear_radius_step import LinearRadiusStep
+from mrca.radius_finder import RadiusFinder
+
+__author__ = 'Emanuele Tamponi'
+
+
+class TestLinearRadiusStep(unittest.TestCase):
+
+    def test_simple_case(self):
+        inputs = numpy.asarray([3, 7, 4, 9, 1, 2, 5]).reshape((7, 1))
+        expected_radii = [2, 3.5, 5]
+        chooser = LinearRadiusStep(RadiusFinder("median"), smallest_size=2, largest_size=5, profile_dim=3)
+        radii = chooser.choose(inputs)
+        self.assertEqual(expected_radii, radii)
+
+    def test_float_size(self):
+        inputs = numpy.asarray([3, 7, 4, 9, 1, 2, 5]).reshape((7, 1))
+        expected_radii = [2, 3.5, 5]
+        chooser = LinearRadiusStep(RadiusFinder("median"), smallest_size=0.3334, largest_size=0.8334, profile_dim=3)
+        radii = chooser.choose(inputs)
+        self.assertEqual(expected_radii, radii)

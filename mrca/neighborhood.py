51041d87bfe27f663d0cbcfa6d1d7b4a2ebf8f39
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-12-11 12:03:19 +0100

    Handle empty neighborhoods and att toggle to exclude center

--- mrca/neighborhood.py
+++ mrca/neighborhood.py
@@ -6,7 +6,8 @@ __author__ = 'Emanuele Tamponi'
 
 class Neighborhood(object):
 
-    def __init__(self, inputs, labels):
+    def __init__(self, inputs, labels, include_center=True):
+        self.include_center = include_center
         self.inputs = inputs.copy()
         self.labels = labels.copy()
         self.distance_matrix = distance.squareform(distance.pdist(inputs))
@@ -14,4 +15,6 @@ class Neighborhood(object):
     def iterate(self, radius):
         for row in self.distance_matrix:
             mask = row <= radius
+            if not self.include_center:
+                mask = numpy.logical_and(mask, row > 0)
             yield (self.inputs[mask], self.labels[mask])
--- mrca/test_neighborhood.py
+++ mrca/test_neighborhood.py
@@ -18,7 +18,9 @@ class TestNeighborhood(unittest.TestCase):
             [2, 1, 0]
         ])
         neigh = Neighborhood(self.inputs, self.labels)
+        self.assertIsNot(self.inputs, neigh.inputs)
         self.assertIsNot(self.labels, neigh.labels)
+        numpy.testing.assert_array_equal(self.inputs, neigh.inputs)
         numpy.testing.assert_array_equal(self.labels, neigh.labels)
         numpy.testing.assert_array_equal(expected_distance_matrix, neigh.distance_matrix)
 
@@ -32,3 +34,23 @@ class TestNeighborhood(unittest.TestCase):
         for i, (inputs, labels) in enumerate(neigh.iterate(radius=1)):
             numpy.testing.assert_array_equal(expected_nh[i][0], inputs)
             numpy.testing.assert_array_equal(expected_nh[i][1], labels)
+
+    def test_include_center(self):
+        neigh = Neighborhood(self.inputs, self.labels, include_center=False)
+        expected_nh = [
+            (numpy.asarray([[6]]), numpy.asarray(["a"])),
+            (numpy.asarray([[5], [7]]), numpy.asarray(["a", "b"])),
+            (numpy.asarray([[6]]), numpy.asarray(["a"]))
+        ]
+        for i, (inputs, labels) in enumerate(neigh.iterate(radius=1)):
+            numpy.testing.assert_array_equal(expected_nh[i][0], inputs)
+            numpy.testing.assert_array_equal(expected_nh[i][1], labels)
+
+    def test_empty(self):
+        neigh = Neighborhood(self.inputs, self.labels, include_center=False)
+        expected_neigh_inputs = numpy.asarray([]).reshape((0, 1))
+        expected_neigh_labels = numpy.asarray([]).astype('|S1')
+        for neigh_inputs, neigh_labels in neigh.iterate(radius=0.5):
+            numpy.testing.assert_array_equal(expected_neigh_inputs, neigh_inputs,
+                                             err_msg="Expected empty, got {}".format(neigh_inputs))
+            numpy.testing.assert_array_equal(expected_neigh_labels, neigh_labels)

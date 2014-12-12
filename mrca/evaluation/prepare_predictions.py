9133824297f367504b0ecdc239a83de08fa282e1
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-12-12 14:51:33 +0100

    Code for preparing predictions

---
+++ mrca/evaluation/prepare_predictions.py
@@ -0,0 +1,40 @@
+import os
+import cPickle
+import numpy
+import sklearn
+from sklearn.cross_validation import StratifiedKFold
+from analysis.dataset_utils import ArffLoader
+from mrca import evaluation
+
+__author__ = 'Emanuele Tamponi'
+
+
+def main():
+    for dataset in evaluation.DATASET_NAMES:
+        predict_dataset(dataset)
+
+
+def predict_dataset(dataset):
+    file_name = "{}_predictions".format(dataset)
+    if os.path.isfile("intermediate/{}.int".format(file_name)):
+        print "{} already done".format(file_name)
+        return
+    print "{} starting".format(file_name)
+
+    inputs, labels = ArffLoader("datasets/{}.arff".format(dataset)).get_dataset()
+    predictions = {key: numpy.zeros_like(labels) for key in evaluation.CLASSIFIER_NAMES}
+    predictions["oracle"] = labels
+    for train_indices, test_indices in StratifiedKFold(labels, n_folds=evaluation.N_FOLDS):
+        train_inputs, train_labels = inputs[train_indices], labels[train_indices]
+        test_inputs, test_labels = inputs[test_indices], labels[test_indices]
+        for name, classifier in evaluation.CLASSIFIERS.iteritems():
+            classifier = sklearn.clone(classifier)
+            predictions[name][test_indices] = classifier.fit(train_inputs, train_labels).predict(test_inputs)
+
+    with open("intermediate/{}.int".format(file_name), "w") as f:
+        cPickle.dump(predictions, f)
+    print "{} saved".format(file_name)
+
+
+if __name__ == '__main__':
+    main()

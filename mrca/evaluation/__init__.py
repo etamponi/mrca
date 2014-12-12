bd0c2628cc1fc20757761a6a9a60a1efd96c1c83
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-12-12 12:31:42 +0100

    Prepare constants

--- mrca/evaluation/__init__.py
+++ mrca/evaluation/__init__.py
@@ -1,1 +1,38 @@
+from sklearn.ensemble.bagging import BaggingClassifier
+from sklearn.ensemble.forest import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
+from sklearn.ensemble.weight_boosting import AdaBoostClassifier
+from mrca.probes.imbalance import Imbalance
+from mrca.probes.linear_boundary import LinearBoundary
+
 __author__ = 'Emanuele Tamponi'
+
+
+CLASSIFIER_NAMES = ["ab", "gb", "ba", "rf", "et"]
+CLASSIFIERS = [
+    AdaBoostClassifier(n_estimators=100),
+    GradientBoostingClassifier(n_estimators=100),
+    BaggingClassifier(n_estimators=100),
+    RandomForestClassifier(n_estimators=100),
+    ExtraTreesClassifier(n_estimators=100)
+]
+
+PROBE_NAMES = ["imb", "lin"]
+PROBES = [
+    Imbalance(),
+    LinearBoundary()
+]
+
+PROFILE_SIZES = range(5, 41, 5)
+
+LEGEND = {
+    "ab": "AdaBoost",
+    "gb": "Grad. Boosting",
+    "ba": "Bagging",
+    "rf": "Random Forest",
+    "et": "Extr. Rand. Trees",
+    "imb": "Imbalance",
+    "lin": "Linear Boundary"
+}
+
+CLUSTER_NUMS = [2, 3, 4, 5, 6]
